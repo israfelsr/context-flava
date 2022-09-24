7ced8e662438dda3ac51bf0a082c23e97ede7135
Israfel Salazar <israfel.sr@gmail.com>
2022-09-24 16:01:33 +0200

    add: added automation name files already processed

--- sbatchPrecontext.sh
+++ sbatchPrecontext.sh
@@ -1,6 +1,3 @@
 export PYTHONPATH=$(pwd)
 python scripts/precontext/sst_to_image.py\
-    --hf_repository=israfelsr/multimodal_sst2\
-    --split=train\
-    --num_chunks=5\
-    --auth_token=
+    config=./configs/precontext/sst2_to_image.yaml
--- scripts/precontext/sst_to_image.py
+++ scripts/precontext/sst_to_image.py
@@ -1,4 +1,3 @@
-import argparse
 import logging
 import sys
 import numpy as np
@@ -8,7 +7,7 @@ from datasets import load_dataset, concatenate_datasets
 from diffusers import StableDiffusionPipeline
 
 from src.precontext.utils import generate_with_diffuser
-from utils import build_general_config
+from src.utils import build_general_config
 
 LOG = logging.getLogger(__name__)
 
@@ -27,14 +26,18 @@ def main():
     latents = torch.randn((1, diffuser.unet.in_channels, 64, 64),
                           generator=generator,
                           device=config.device)
-    chunks = np.linspace(0, 100, config.num_chunks)
+    chunks = np.linspace(0, 100, config.num_chunks + 1)
     dataset_stats = {
-        "split": config.split,
-        "num_chunks": config.num_chunks,
-        "chunks": chunks,
-        "total_chunks": len(chunks - 1),
-        "num_chunk_processed": config.num_chunk_processed,
-        "splits_in_repo": config.splits_in_repo,
+        "split":
+        config.split,
+        "num_chunks":
+        config.num_chunks,
+        "chunks":
+        chunks,
+        "num_chunks_processed":
+        config.num_chunk_processed,
+        "splits_in_repo":
+        [f"{config.split}_{t}" for t in range(config.num_chunks_processed)],
     }
 
     for t, (start, stop) in enumerate(zip(chunks[:-1], chunks[1:])):
@@ -51,36 +54,35 @@ def main():
                                    batched=True,
                                    batch_size=config.batch_size)
         LOG.info(f"Dataset chunk processing finished")
-        LOG.info(f"Pushing chunk to HF respository: {config.hf_repository}")
+        LOG.info(f"Pushing chunk to HF respository: {config.repo_id}")
         repo_split = f"{config.split}_{t}"
         LOG.info(f"Storing the chunk using the split: {repo_split}")
         multimodal_sst2.push_to_hub(
-            repo_id=config.hf_repository,
+            repo_id=config.repo_id,
             split=repo_split,
             private=True,
             token=config.auth_token,
         )
         dataset_stats["num_chunks_processed"] += 1
         LOG.info(
-            f"{dataset_stats['num_chunks_processed']}/{dataset_stats['total_chunks']} Chunks processed"
+            f"{dataset_stats['num_chunks_processed']}/{config.num_chunks} Chunks processed"
         )
         dataset_stats["splits_in_repo"].append(repo_split)
         LOG.info(dataset_stats)
 
     if config.do_merge:
-        assert dataset_stats["num_chunks_processed"] == dataset_stats[
-            "total_chunks"]
+        assert dataset_stats["num_chunks_processed"] == config.num_chunks
         LOG.info(f"All chunks processed, merging into split: {config.split}")
-        prev_data = load_dataset(repo_id=config.hf_repository,
+        prev_data = load_dataset(config.repo_id,
                                  split=dataset_stats["splits_in_repo"][0],
                                  use_auth_token=config.auth_token)
         for next_split in dataset_stats["splits_in_repo"][1:]:
-            next_data = load_dataset(repo_id=config.hf_repository,
+            next_data = load_dataset(config.repo_id,
                                      split=next_split,
                                      use_auth_token=config.auth_token)
             prev_data = concatenate_datasets([prev_data, next_data])
         LOG.info("Pushing all merged dataset to hub")
-        prev_data.push_to_hub(repo_id=config.hf_repository,
+        prev_data.push_to_hub(repo_id=config.repo_id,
                               split=config.split,
                               private=True,
                               token=config.auth_token)
