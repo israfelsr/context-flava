20467dd3a7c5f4aa8a17ca80c28675685ecfa627
Israfel Salazar <israfel.sr@gmail.com>
2022-09-29 11:12:09 +0200

    solving nsfw images turn to none or black if indicated

--- src/precontext/utils.py
+++ src/precontext/utils.py
@@ -19,21 +19,29 @@ def generate_with_diffuser(batch, diffuser: StableDiffusionPipeline,
         )
     idx = 0
     while any(image["nsfw_content_detected"]):
+        idx += 1
+        if idx == args.max_nsfw_tries:
+            LOG.info("NSFW detected and max number of tries reached")
+            if args.set_to_none:
+                nsfw_idx = [
+                    i for i, x in enumerate(image["nsfw_content_detected"])
+                    if x
+                ]
+                image["sample"][nsfw_idx] = None
+                LOG.info("Image set to None")
+            else:
+                LOG.info("Image set to black")
+            break
         generator = generator.manual_seed(generator.seed() + 1)
         latents = torch.randn((1, diffuser.unet.in_channels, 64, 64),
                               generator=generator,
                               device="cuda")
         with torch.autocast("cuda"):
-            idx = +1
             image = diffuser(
                 batch["sentence"],
                 guidance_scale=7.5,
                 latents=latents,
             )
-        if idx == args.max_nsfw_tries:
-            LOG.info("NSFW detected and max number of tries reached")
-            LOG.info("Image set to black")
-            break
     generator.manual_seed(args.seed)
     batch["image"] = image["sample"]
     return batch
