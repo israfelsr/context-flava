6ce8dbb5222a142a699eb510d659ca580dc653a8
Israfel Salazar <israfel.sr@gmail.com>
2022-11-24 16:41:11 +0100

    update

--- sbatchRunFLAVA.sh
+++ sbatchRunFLAVA.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=$(pwd)
 export PYTHONPATH=../context-flava/multimodal/examples/:$PYTHONPATH
 export PYTHONPATH=../context-flava/multimodal/:$PYTHONPATH
-python scripts/run_sst.py\
+python scripts/run_flava.py\
     config=./configs/finetuning/mm_sst2_test.yaml\
     model.pretrained_model_key=flava_full
